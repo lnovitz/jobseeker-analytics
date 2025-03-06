841129010e4ce96a7566acebd9b3e7c0fc681374
kevinlambchops <lamkevin7777@gmail.com>
2025-03-05 22:44:32 -0500

    added user_is_in_database function

--- backend/db/utils/user_utils.py
+++ backend/db/utils/user_utils.py
@@ -1,23 +1,38 @@
 import logging
 from sqlmodel import Session, select
 from users import Users 
+from datetime import datetime, timedelta, timezone
 
 logger = logging.getLogger(__name__)
 
-def add_user(user, session: Session) -> Users:
+def user_is_in_database(user, session: Session) -> bool:
     """
-    Writes user data to the users model upon login.
+    Checks if user is already in the database
 
     """
-    # check if the user already exists in the database
     existing_user = session.exec(select(Users).where(Users.user_id == user.user_id)).first()
     if not existing_user:
-        # if not we just create a new user record
+        return False
+    else:
+        return True
+
+def add_user(user, start_date: datetime, session: Session) -> Users:
+    """
+    Writes user data to the users model
+
+    """
+    # Check if the user already exists in the database
+    existing_user = session.exec(select(Users).where(Users.user_id == user.user_id)).first()
+
+    if not existing_user:
+        # add a new user record
         new_user = Users(
             user_id=user.user_id,
             user_email=user.user_email,
-            google_openid=user.google_openid
+            google_openid=user.google_openid,
+            start_date=start_date
         )
+
         session.add(new_user)
         session.commit()
         session.refresh(new_user)
