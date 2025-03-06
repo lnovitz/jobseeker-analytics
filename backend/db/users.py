67ba4e0b5ea88a62a8d0f4c6c4a00087958bcaca
kevinlambchops <lamkevin7777@gmail.com>
2025-03-05 22:40:47 -0500

    added start_date attribute

--- backend/db/users.py
+++ backend/db/users.py
@@ -1,7 +1,9 @@
 from sqlmodel import SQLModel, Field
+from datetime import datetime, timedelta, timezone
 
 class Users(SQLModel, table=True):
     __tablename__ = "users"
     user_id: int = Field(default = None, primary_key = True)
     user_email: str = Field(nullable=False)                      
     google_openid: str = Field(nullable=False, unique=True) # OpenID identifier
+    start_date: datetime = Field(nullable=False) # Start date for job applications
